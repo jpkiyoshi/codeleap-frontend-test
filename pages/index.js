dee108f17bf35d103020f1b3f97a9de91264eed6
João P. Kiyoshi <kiyoshi33@gmail.com>
2023-04-12 13:34:55 -0300

    add page titles

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import SignUpModal from '@/components/SignUpModal';
+import Head from 'next/head';
 
 export default function Home() {
 	return (
-		<main className='grid min-h-screen p-6 place-content-center'>
-			<SignUpModal />
-		</main>
+		<>
+			<Head>
+				<title>CodeLeap Engineering Test</title>
+			</Head>
+			<main className='grid min-h-screen p-6 place-content-center'>
+				<SignUpModal />
+			</main>
+		</>
 	);
 }
--- pages/main-screen.js
+++ pages/main-screen.js
@@ -1,10 +1,14 @@
 import CreatePost from '@/components/CreatePost';
 import Header from '@/components/Header';
 import PostCards from '@/components/PostCards';
+import Head from 'next/head';
 
 const MainScreen = () => {
 	return (
 		<>
+			<Head>
+				<title>CodeLeap Engineering Test</title>
+			</Head>
 			<Header />
 			<main className='max-w-[800px] mx-auto flex flex-col bg-white p-6 gap-6'>
 				<CreatePost />
